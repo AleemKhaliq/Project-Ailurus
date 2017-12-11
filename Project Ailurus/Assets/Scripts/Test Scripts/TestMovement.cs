881ce9595433411370a0c0b5c906231daf2b8125
Aleem Khaliq <aleemkhaliq@ymail.com>
2017-12-11 17:28:52 +0000

    Redirection+Base Movement
    Beginning to shift the direction of the game from a ragdoll to simple
    geometry. implementing testing of player controls and movement physics

---
+++ Project Ailurus/Assets/Scripts/Test Scripts/TestMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour {
+
+    public float boxSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //BaseMovement();
+	}
+
+    // Fixed Update is called before every physics calculation
+    private void FixedUpdate()
+    {
+
+    }
+
+    //Base movement for testing
+    void BaseMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(-boxSpeed, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(boxSpeed, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(0, 0, -boxSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0, 0, boxSpeed);
+        }
+    }
+
+    void Hop()
+    {
+
+    }
+}
